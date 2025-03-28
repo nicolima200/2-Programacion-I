cba1995e95e8e8c1fd38a3abc7aa5b801f554f7e
nicolima200 <nicolima200@gmail.com>
2025-03-28 09:53:20 -0300

    Copié lineas y las agruegué

--- SEMANA 1/ejemplo.py
+++ SEMANA 1/ejemplo.py
@@ -1,9 +1,12 @@
+
 nombre=input("Ingrese seu nombre:")
 apellido=input("Ingrese seu apellido:")
-
 print(f"Hola {nombre} {apellido}")
 
 nombre=input("Ingrese seu nombre:")
 apellido=input("Ingrese seu apellido:")
+print(f"Hola {nombre} {apellido}")
 
+nombre=input("Ingrese seu nombre:")
+apellido=input("Ingrese seu apellido:")
 print(f"Hola {nombre} {apellido}")
