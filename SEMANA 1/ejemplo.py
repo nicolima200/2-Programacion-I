5a41ba0d7cd485fff2d4737eebb4e1ad724f250d
nicolima200 <nicolima200@gmail.com>
2025-03-28 09:47:46 -0300

    Modificado ejemplo.py

--- SEMANA 1/ejemplo.py
+++ SEMANA 1/ejemplo.py
@@ -2,3 +2,8 @@ nombre=input("Ingrese seu nombre:")
 apellido=input("Ingrese seu apellido:")
 
 print(f"Hola {nombre} {apellido}")
+
+nombre=input("Ingrese seu nombre:")
+apellido=input("Ingrese seu apellido:")
+
+print(f"Hola {nombre} {apellido}")
